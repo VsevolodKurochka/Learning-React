6c5a3e44b81ce11dcc80ed6709179af4adc685fb
VsevolodKurochka <dramdoobas123456@gmail.com>
2017-10-07 16:43:15 +0300

    Create Button Component with changing class onclick

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 class Person extends React.Component{
 	render(){
 		return	<div>
-							<p>Name: {this.props.name}</p>
-							<p>Old: {this.props.lastName}</p>
+							<h1>Name: {this.props.name}</h1>
+							<h2>Old: {this.props.lastName}</h2>
 						</div>
 	}
 }
@@ -10,5 +10,34 @@ Person.defaultProps = {
 	name: 'Vsevolod',
 	lastName: 'Kurochka'
 }
-ReactDOM.render(<Person />, document.getElementById('container'));
+ReactDOM.render(<Person />, document.getElementById('person'));
 //getElementById('container')
+
+// Button
+
+class Button extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			class: "btn-danger",
+			text: 'Кнопка OFF'
+		}
+
+		this.click = this.click.bind(this);
+	}
+	click(e){
+		console.log(e);
+		var className = (this.state.class == "btn-danger") ? "btn-success" : "btn-danger";
+		var text = (this.state.class == "btn-danger") ? "Кнопка ON" : "Кнопка OFF";
+		this.setState({
+			class: className,
+			text: text
+		});
+	}
+	render(){
+		return <button onClick={this.click} className={'btn  ' + this.state.class}>{this.state.text}</button>
+	}
+}
+
+ReactDOM.render(<Button />, document.getElementById('button'));
